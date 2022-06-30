0653fa97fde2c3871120e5394675bc53bd24deea
Raival-e <se.10.sec.ac.180@gmail.com>
2022-06-30 13:28:18 +0400

    Add method in BottomBarView that allow adding custom items

--- app/src/main/java/com/raival/fileexplorer/common/view/BottomBarView.java
+++ app/src/main/java/com/raival/fileexplorer/common/view/BottomBarView.java
@@ -7,16 +7,11 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.raival.fileexplorer.R;
 
-import java.util.ArrayList;
-
 public class BottomBarView extends LinearLayout {
-    private final ArrayList<Item> items = new ArrayList<>();
-
     public BottomBarView(Context context) {
         super(context);
     }
@@ -29,44 +24,23 @@ public class BottomBarView extends LinearLayout {
         super(context, attrs, defStyleAttr);
     }
 
-    public BottomBarView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-    }
-
-    public void addItem(@NonNull String tag, int icon, OnClickListener clickListener) {
+    public void addItem(String tag, int icon, OnClickListener clickListener) {
         View view = ((LayoutInflater) getContext()
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE))
                 .inflate(R.layout.bottom_bar_menu_item, this, false);
         view.setOnClickListener(clickListener);
+        view.setTooltipText(tag);
         ImageView image = view.findViewById(R.id.icon);
         image.setImageResource(icon);
-        items.add(new Item(tag, view, image));
         addView(view);
     }
 
-    public Item getItem(String tag) {
-        for (Item item : items) {
-            if (item.tag.equals(tag)) {
-                return item;
-            }
-        }
-        return null;
+    public void addItem(String tag, View view) {
+        view.setTooltipText(tag);
+        addView(view);
     }
 
     public void clear() {
-        items.clear();
         removeAllViews();
-    }
-
-    public class Item {
-        String tag;
-        View view;
-        ImageView icon;
-
-        public Item(String tag, View view, ImageView icon) {
-            this.tag = tag;
-            this.view = view;
-            this.icon = icon;
-        }
     }
 }
