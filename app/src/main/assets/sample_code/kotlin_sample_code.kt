4eec961e32d851204ff721b07a81f65a16f37728
Raival-e <se.10.sec.ac.180@gmail.com>
2022-07-11 08:29:34 +0400

    Update kotlin_sample_code.kt

--- app/src/main/assets/sample_code/kotlin_sample_code.kt
+++ app/src/main/assets/sample_code/kotlin_sample_code.kt
@@ -4,8 +4,8 @@ import android.content.Context
 import android.widget.Toast
 import java.io.File
 
-object Main {
-	@JvmStatic fun main(context: Context, directory: File) {
+class Main(context: Context, directory: File) {
+	init {
 		Toast.makeText(context, "Hello World!", Toast.LENGTH_SHORT).show();
 	}
 }
