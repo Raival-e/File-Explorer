ed48565f4535d80c5ee56cc01b6c497e14af1d51
Raival-e <se.10.sec.ac.180@gmail.com>
2022-07-22 16:56:49 +0400

    Add jpg file extension

--- app/src/main/java/com/raival/fileexplorer/tab/file/util/FileExtensions.java
+++ app/src/main/java/com/raival/fileexplorer/tab/file/util/FileExtensions.java
@@ -6,7 +6,7 @@ public class FileExtensions {
     public final static String[] archiveType = {"zip", "7z", "tar", "jar", "gz", "xz", "xapk", "obb", "apk", "rar"};
     public final static String[] textType = {"txt", "text", "log", "dsc", "apt", "rtf", "rtx"};
     public final static String[] codeType = {"java", "xml", "py", "css", "kt", "cs", "xml", "json", "svg"};
-    public final static String[] imageType = {"png", "jpeg", "heic", "tiff", "gif", "webp", "svg", "bmp"};
+    public final static String[] imageType = {"png", "jpeg", "jpg", "heic", "tiff", "gif", "webp", "svg", "bmp"};
     public final static String apkType = "apk";
     public final static String rarType = "rar";
     public final static String pdfType = "pdf";
